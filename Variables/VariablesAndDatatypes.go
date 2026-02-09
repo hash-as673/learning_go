fdb73cf4470205a206ef8a2cd82a9c32dd00f6b4
Aditya Singh <as1671109@gmail.com>
2026-02-09 20:29:55 +0530

    began programming in go and done till functions

---
+++ Variables/VariablesAndDatatypes.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+)
+
+var i,j int = 1,2 
+func main(){
+	// var i,j int = 1,2 not allowed here
+	var c,python,java = false, false, true
+	fmt.Println(i,j,c,python,java)
+
+
+	//Short variable declaration
+}
